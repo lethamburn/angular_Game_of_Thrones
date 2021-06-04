b3c394557608eb53b84ae4273f0c2611dde80091
frank <frankestrada41@hotmail.com>
2021-06-04 21:55:52 +0200

    paginaPersonajes

--- src/app/shared/services/characters.service.ts
+++ src/app/shared/services/characters.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharactersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCharacters() {
+
+    return this.http.get('https://api.got.show/api/show/characters/');
+  }
+
+
 }
