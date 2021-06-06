c51909a99bc05cba132b7f6aa19b287a56f8076a
frank <frankestrada41@hotmail.com>
2021-06-06 22:55:09 +0200

    ordenedad

--- src/app/shared/components/chronology-gallery/chronology-gallery.component.ts
+++ src/app/shared/components/chronology-gallery/chronology-gallery.component.ts
@@ -7,30 +7,41 @@ import { CharactersService } from '../../services/characters.service';
   styleUrls: ['./chronology-gallery.component.scss']
 })
 export class ChronologyGalleryComponent implements OnInit {
-
+  characterOrder: any[] = [];
   constructor(private charactersService: CharactersService) { }
   textfilter: any;
-  @Input() chronologyList: any[] = [];
+
+  // @Input() chronologyList: any[] = [];
   @Input() characters: any[] = [];
 
   ngOnInit(): void {
     this.charactersService.getCharacters().subscribe((charactersData: any) => {
       console.log(charactersData);
-      for (const characterData of charactersData) {
-        if(characterData.age.age){
-      const charactersort = charactersData;
-      console.log(charactersort[0].age.age);
-      this.characters = charactersort.sort();
-    }};
+      for (let characterData of charactersData) {
+        if (characterData.age && characterData.age.age) {
+          // const charactersort = charactersData;
+          this.characterOrder.push(characterData)
+        }
+      };
+      this.listCharactersAsc(this.characterOrder)
+      console.log(this.characterOrder)
+    }
+    )
   }
-    )}};
 
-/* function compare( a:any, b:any ) {
-  if ( a.age.age < b.age.age ){
-    return -1;
+  listCharactersAsc(listCharacters: any) {
+    listCharacters.sort(function (a: any, b: any) {
+      if (a.age.age > b.age.age) {
+        return 1;
+      }
+      if (a.age.age < b.age.age) {
+        return -1;
+      }
+      return 0;
+    });
+
   }
-  if ( a.age.age > b.age.age ){
-    return 1;
-  }
-  return 0;
-} */
+
+
+};
+
